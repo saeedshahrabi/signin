7cbea6cb7c7e9ae1856f5876ae12868d08c5ff47
s.shahrabi <saeed50002000@gmail.com>
2022-06-11 16:11:13 -0400

    routes and initial language was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter, Routes, Outlet, Navigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import Signin from "./pages/Signin";
+import { RootState } from "./redux/store";
+import { useSelector } from "react-redux";
+import NoMatch from "./pages/NoMatch";
+import { Localization } from "./config/translate/localization";
+import Layout from "./component/Layout";
 
-function App() {
+const App = () => {
+  const store: any = useSelector((state: RootState) => state);
+
+  // set language on language change and on init
+  const currentFlag = Localization.getLanguage();
+  const storedFlag = store.LanguageReducer.flag;
+
+  if (currentFlag !== storedFlag) {
+    if (store.LanguageReducer.flag === "fr") {
+      Localization.setLanguage("fr");
+    } else {
+      Localization.setLanguage("en");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // our routes
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="signin" element={<Signin />} />
+            <Route path="/" element={<Navigate replace to="/signin" />} />
+          </Route>
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+        <Outlet />
+      </BrowserRouter>
   );
-}
+};
 
 export default App;
